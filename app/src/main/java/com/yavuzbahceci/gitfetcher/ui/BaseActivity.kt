26fcd6f927289f1af9331930736fd90dd5823339
yavuzBahceci <yavuz2991>
2020-10-02 19:30:23 +0300

    base activity and fragment created.

---
+++ app/src/main/java/com/yavuzbahceci/gitfetcher/ui/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.yavuzbahceci.gitfetcher.ui
+
+import dagger.android.support.DaggerAppCompatActivity
+
+abstract class BaseActivity: DaggerAppCompatActivity(){
+
+    private val TAG: String = "AppDebug"
+
+
+
+}
---
+++ app/src/main/java/com/yavuzbahceci/gitfetcher/ui/main/BaseMainFragment.kt
@@ -0,0 +1,27 @@
+package com.yavuzbahceci.gitfetcher.ui.main
+
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import com.yavuzbahceci.gitfetcher.view_models.ViewModelProviderFactory
+import dagger.android.support.DaggerFragment
+import javax.inject.Inject
+
+abstract class BaseMainFragment: DaggerFragment(){
+
+    val TAG: String = "AppDebug"
+
+    @Inject
+    lateinit var providerFactory: ViewModelProviderFactory
+
+    lateinit var viewModel: MainViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = activity?.run {
+            ViewModelProvider(this, providerFactory).get(MainViewModel::class.java)
+        } ?: throw Exception("Invalid Activity")
+    }
+
+}
